1dcec50e4d2254f5b64b2869d29d8d71412f9f13
Emily Ritter <emily.ritter@warrendouglas.com>
2017-06-12 16:47:36 -0500

    Use ReactRouter to navigate to the store page when user submits the form on the homepage (Videos 11 & 12)

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,18 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event){
+    event.preventDefault();
+    const storeId = this.storeInput.value;
+    this.context.router.transitionTo(`/store/${storeId}`);
+  }
   render(){
     // Example of a comment in the render function
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
       { /* Example of a comment in the return statement */ }
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name"
-          defaultValue={getFunName()} />
+        <input type="text" required placeholder="Store Name" defaultValue={
+            getFunName()} ref={(input) => {this.storeInput = input}}/>
         <button type="submit">Visit Store -> </button>
       </form>
     )
   }
+}
+StorePicker.contextTypes = {
+  router: React.PropTypes.object
 }
 
 export default StorePicker;
