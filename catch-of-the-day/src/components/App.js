adb200a03eea703ea36e665921e148f6f5db8480
Emily Ritter <emily.ritter@warrendouglas.com>
2017-06-13 17:00:48 -0500

    Video 13: AddFishForm component - intro to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,31 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-  render(){
+  constructor() {
+    super();
+    this.addFish = this.addFish.bind(this);
+    this.state = {
+      fishes: {},
+      order: {}
+    }
+  }
+  addFish(fish) {
+    // get the current state
+    const fishes = {...this.state.fishes};
+    // add our fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({fishes});
+  }
+  render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header tagline="Fresh Seafood Market"/>
+          <Header tagline="Fresh Seafood Market"  />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     )
   }
