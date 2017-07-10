6d0356b1dc5ee09e3d3dcb7554be8185dd29e0f1
Emily Ritter <emily.ritter@warrendouglas.com>
2017-07-10 10:29:08 -0500

    Video 16 - alternate syntax

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,7 +3,7 @@ import {formatPrice} from '../helpers'
 
 class Fish extends React.Component {
   render(){
-    const {details} = this.props;
+    const {details, index} = this.props;
     const isAvailable = details.status === 'available';
     const buttonText = isAvailable ? 'Add to Order' : 'Sold Out!';
 
@@ -15,7 +15,7 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(details.price)}</span>
         </h3>
         <p>{details.desc}</p>
-        <button disabled={!isAvailable} onClick={this.props.addToOrder.bind(null,this.props.index)}>{buttonText}</button>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(index)}>{buttonText}</button>
       </li>
     )
   }
